e5fb156fa10d257463ff485246dec4f8e36e2331
Kawashiro Shizuru <KappaShizuru@gmail.com>
2026-05-15 15:50:47 +0800

    "Changed Blog Name"

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const SITE = {
   author: "Sat Naing",
   profile: "https://satnaing.dev/",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "KappaNet Blog",
+  title: "Kawashiro",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: false,
   postPerIndex: 4,
